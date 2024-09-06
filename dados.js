bd3ff05847cd21d59ff3f49b568924201bd5e241
Maski Yofem <maxlucasaleixo@gmail.com>
2024-09-06 14:24:32 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,25 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal é uma jovem skatista brasileira que conquistou o mundo com seu talento e carisma.Conhecida como Fadinha do Skate ela se tornou a mais jovem medalhista olímpicada história do Brasil, ao conquistar a prata nos Jogos Olímpicos de Tóquio 2020.",
+        link:"https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte.",
+        link:"https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    },
+   
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que vem se destacando no cenário internacional. Com sua força, técnica e determinação, ela conquistou diversos títulos importantes e se tornou uma das principais esperanças de medalhas para o Brasil em competições como os Jogos Olímpicos.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    }
+];
+
+
+
